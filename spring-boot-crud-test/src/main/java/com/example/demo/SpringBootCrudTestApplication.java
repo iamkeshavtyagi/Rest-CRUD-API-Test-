d4b7fcffbf93feee602effe565af5f72c8cae2e2
KESHAV TYAGI <60292531+iamkeshavtyagi@users.noreply.github.com>
2022-03-04 16:18:34 +0530

    Add files via upload

---
+++ spring-boot-crud-test/src/main/java/com/example/demo/SpringBootCrudTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudTestApplication.class, args);
+	}
+
+}
